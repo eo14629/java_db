231fb7b03b1487b4671354d365542d472fe6a113
Eddie O'Donnell <eddieod88@gmail.com>
2018-02-25 18:22:23 +0000

    changed from arrayList to Hashtable as this made it possible to distinguish between records via the use of keys

--- Record.java
+++ Record.java
@@ -1,7 +1,12 @@
+// make a Hashtable which has an integer key and a variable length array of strings for input.
+// This way any number of fields can be inputted but this could be set to a specific value.
+// Also, each record has its unique key so it is easy to retrieve the record.
+
 import java.util.*;
 
 class Record {
-  private ArrayList<String> recordList = new ArrayList<String>();
+  private Hashtable<Integer,ArrayList<String>> recordTable = new Hashtable<Integer,ArrayList<String>>();
+  private int key = 0;
 
   public static void main(String args[]) {
     Record program = new Record();
@@ -9,16 +14,22 @@ class Record {
   }
 
   void addRecord(String... field_entries) {
-
+    ArrayList<String> new_record = new ArrayList<String>();
     for (String field_entry: field_entries) {
-      recordList.add(field_entry);
+      new_record.add(field_entry);
     }
-    System.out.println(recordList + " & size = " + recordList.size());
+    recordTable.put(key, new_record);
+    key++;
+  }
+
+  void printHashtable() {
+    System.out.println(recordTable);
   }
 
   void testRecord() {
     addRecord("One","Two","Three");
     addRecord("a","b");
     addRecord("five","m","i","l","l","i","o","n");
+    printHashtable();
   }
 }
