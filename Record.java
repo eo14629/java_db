fd4ad6b75e0b64ce60f2a546e7a4bb9ee2d99ece
Eddie O'Donnell <eddieod88@gmail.com>
2018-02-25 19:16:29 +0000

    included getitem, getrecord and ammend methods. the error throwing in the ammend method could be improved

--- Record.java
+++ Record.java
@@ -5,7 +5,7 @@
 import java.util.*;
 
 class Record {
-  private Hashtable<Integer,ArrayList<String>> recordTable = new Hashtable<Integer,ArrayList<String>>();
+  private Hashtable<Integer,ArrayList<String>> records = new Hashtable<Integer,ArrayList<String>>();
   private int key = 0;
 
   public static void main(String args[]) {
@@ -18,12 +18,34 @@ class Record {
     for (String field_entry: field_entries) {
       new_record.add(field_entry);
     }
-    recordTable.put(key, new_record);
+    records.put(key, new_record);
     key++;
   }
 
+  ArrayList<String> getRecord(int id_key) {
+    return records.get(id_key);
+  }
+
+  String getItem(int id_key, int field_num) {
+    return getRecord(id_key).get(field_num);
+  }
+
+  void ammendRecord(int id_key, int field_num, String value) {
+    // if (field_num < getRecord(id_key).size()) {
+      getRecord(id_key).set(field_num, value);// is it best to use the set error or create your own?
+    // } else { throw new Error("field_num value larger than num of fields"); }
+  }
+
   void printHashtable() {
-    System.out.println(recordTable);
+    System.out.println(records);
+  }
+
+  /******************************************/
+  /***************** TESTING ****************/
+  /******************************************/
+
+  void claim(boolean b) {
+    if (!b) throw new Error("Test failed");
   }
 
   void testRecord() {
@@ -31,5 +53,14 @@ class Record {
     addRecord("a","b");
     addRecord("five","m","i","l","l","i","o","n");
     printHashtable();
+
+    claim(getItem(1,1).equals("b"));
+    claim(getItem(2,7).equals("n"));
+
+    claim(getRecord(1).get(0).equals("a"));
+    claim(getRecord(2).get(0).equals("five"));
+
+    ammendRecord(2,0,"six");
+    claim(getItem(2,0).equals("six"));
   }
 }
