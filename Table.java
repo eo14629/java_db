9b847e7253d9f90f10a6fc1b2de2979b63d1a9f0
Eddie O'Donnell <eo14629@my.bristol.ac.uk>
2018-03-08 12:27:13 +0000

    made testing neater and allowed deleteRecord and updateRecord to be selected by pk

--- Table.java
+++ Table.java
@@ -10,7 +10,7 @@ class Table {
   private int id = 0;
 
   public static void main(String args[]) {
-    Table program = new Table();// the * is needed for testing purposes only
+    Table program = new Table();
     program.testTable();
   }
 
@@ -34,6 +34,17 @@ class Table {
     this.headings = field_name_record;
   }
 
+  int numPks() {
+    return pk_cols.size();
+  }
+
+  boolean isPk(String s) {
+    if (s.charAt(s.length()-1) == '*') {
+      return true;
+    }
+    return false;
+  }
+
   // probs need errors here.
   boolean insertRecord(Record r) {
     if (r.size() == headings.size()) {
@@ -51,17 +62,6 @@ class Table {
     return false;
   }
 
-  int numPks() {
-    return pk_cols.size();
-  }
-
-  boolean isPk(String s) {
-    if (s.charAt(s.length()-1) == '*') {
-      return true;
-    }
-    return false;
-  }
-
   ArrayList<String> keyGen(String... keys) {
     ArrayList<String> pks = new ArrayList<String>();
     for (String key : keys) {
@@ -70,12 +70,7 @@ class Table {
     return pks;
   }
 
-  // use the PK for this
-  boolean deleteRecord(String... id_keys) {
-    ArrayList<String> pk = new ArrayList<String>();
-    for (String id_key : id_keys) {
-      pk.add(id_key);
-    }
+  boolean deleteRecord(ArrayList<String> pk) {
     if (table.remove(pk) == null) { return false; }
     return true;
   }
@@ -84,11 +79,10 @@ class Table {
     return table.get(pk);
   }
 
-  // use the PK for this too
-  boolean updateRecord(Record r, String field_name, String value) {
+  boolean updateRecord(ArrayList<String> pk, String field_name, String value) {
     if (headings.contains(field_name)) {
       int i = headings.indexOf(field_name);
-      r.ammendItem(i,value);
+      selectRecord(pk).ammendItem(i,value);
       return true;
     } else {
       return false;
@@ -130,15 +124,6 @@ class Table {
     }
   }
 
-  String listToString(ArrayList<String> al) {
-    StringBuilder sb = new StringBuilder();
-    for (String s: al) {
-      sb.append(s + ",");
-    }
-    sb.deleteCharAt(sb.length()-1);
-    return sb.toString();
-  }
-
   /******************************************/
   /***************** TESTING ****************/
   /******************************************/
@@ -155,7 +140,27 @@ class Table {
     System.out.println("Testing start");
     Table a_table = new Table("Name*", "League");
 
-    // Insertions
+    testInsertions(a_table);
+    testDuplicates(a_table);
+    testDeletions(a_table);
+    testSelections(a_table);
+    testUpdates(a_table);
+
+    Table table_pk = new Table("Name*      ","League* ","Points   ");
+    testNoPks();
+    testPks(table_pk);
+    testMultiPk(table_pk);
+    testOverride(table_pk);
+
+    // test print from the Io class
+    Io io = new Io();
+    io.printTable(table_pk);
+
+    System.out.println("Testing finished");
+  }
+
+  // Insertions
+  void testInsertions(Table a_table) {
     Record r1 = new Record("Bristol FC", "Championship");
     claim(a_table.insertRecord(r1));
     Record r2 = new Record("Wolves", "Championship", "SLDKA");
@@ -168,37 +173,49 @@ class Table {
     Record r5 = new Record("WBA", "Championship");
     claim(a_table.insertRecord(r5));
     claim(a_table.selectRecord(a_table.keyGen("WBA")).getItem(0).equals("WBA"));
+  }
 
-    // checks that duplicate pks cannot get added
+  // checks that duplicate pks cannot get added
+  void testDuplicates(Table a_table) {
     Record r6 = new Record("WBA", "Prem");
     claim(! a_table.insertRecord(r6));
+  }
 
-    // deletions
-    claim(a_table.deleteRecord("WBA"));
-    claim(! a_table.deleteRecord("Rovers"));
+  // deletions
+  void testDeletions(Table a_table) {
+    claim(a_table.deleteRecord(a_table.keyGen("WBA")));
+    claim(! a_table.deleteRecord(a_table.keyGen("Rovers")));
+    claim(a_table.size()==3);
+  }
 
-    // selecting a record
+  // selecting a record
+  void testSelections(Table a_table) {
     claim(a_table.selectRecord(a_table.keyGen("WBA"))==null);
     claim(a_table.selectRecord(a_table.keyGen("Rovers"))==null);
     claim(a_table.selectRecord(a_table.keyGen("Tottenham"))==null);
     claim(a_table.selectRecord(a_table.keyGen("Bristol FC")) != null);
+  }
 
-    // adding and removing columns and updating the values
-    // a_table.printTable();
-    claim(a_table.headings.size() == 2);
+  // adding and removing columns and updating the values
+  void testUpdates(Table a_table) {
+    claim(a_table.headings.size()==2);
     a_table.addColumn("Points");
-    claim(a_table.headings.size() == 3);
+    claim(a_table.headings.size()==3);
     claim(a_table.selectRecord(a_table.keyGen("Burnley")).size() == 3);
-    claim(a_table.updateRecord(r3, "Points", "31"));
-    claim(! a_table.updateRecord(r4, "Poits", "31"));
-    // a_table.printTable();
+    claim(a_table.selectRecord(a_table.keyGen("QPR")).getItem(2)==null);
+    claim(a_table.updateRecord(a_table.keyGen("QPR"), "Points", "31"));
+    claim(a_table.selectRecord(a_table.keyGen("QPR")).getItem(2)!=null);
+    claim(a_table.selectRecord(a_table.keyGen("QPR")).getItem(2).equals("31"));
+    claim(! a_table.updateRecord(a_table.keyGen("Burnley"), "Poits", "31"));
     claim(a_table.removeColoumn("League"));
     claim(! a_table.removeColoumn("Legue"));
+    claim(a_table.headings.size()==2);
+  }
 
-    // check that the primary key column array list stores the correct columns
-    // respresenting the primary keys.
-    // Also make sure that the constructor gets rid of extra spaces
-    Table table_pk = new Table("Name*      ","League* ","Points   ");
+  // check that the primary key column array list stores the correct columns
+  // respresenting the primary keys.
+  // Also make sure that the constructor gets rid of extra spaces
+  void testPks(Table table_pk) {
     claim(table_pk.pk_cols.size()==2);
     claim(table_pk.pk_cols.get(0)==0);
     claim(table_pk.pk_cols.get(1)==1);
@@ -207,12 +224,11 @@ class Table {
     claim(table_pk.headings.getItem(0).equals("Name*"));
     claim(table_pk.headings.getItem(1).equals("League*"));
     claim(table_pk.headings.getItem(2).equals("Points"));
+  }
 
-    // check the error is thrown for no primary keys
-    // Table table_pk2 = new Table("Name","League","Points");
-
-    // ensure the primary keys work as they should and not allow a record
-    // to be inserted if it disobeys the pk rules
+  // ensure the primary keys work as they should and not allow a record
+  // to be inserted if it disobeys the pk rules
+  void testMultiPk(Table table_pk) {
     Record r01 = new Record("Bristol FC", "Championship", "14");
     Record r02 = new Record("Bristol FC", "Premier League", "14");
     Record r03 = new Record("QPR", "Championship", "1");
@@ -222,19 +238,19 @@ class Table {
     claim(table_pk.insertRecord(r03));
     claim(! table_pk.insertRecord(r04));
     claim(table_pk.size()==3);
+  }
 
-    // Ensuring no overriding when entering a duplicate - the record
-    // should not be entered into the table
+  // Ensuring no overriding when entering a duplicate - the record
+  // should not be entered into the table
+  void testOverride(Table table_pk) {
     ArrayList<String> bristol = new ArrayList<String>();
     bristol = table_pk.keyGen("Bristol FC","Premier League");
     claim(! table_pk.selectRecord(bristol).getItem(2).equals("12"));
     claim(table_pk.selectRecord(bristol).getItem(2).equals("14"));
+  }
 
-    // a_table.printTable();
-
-    Io io = new Io();
-    io.printTable(table_pk);
-
-    System.out.println("Testing finished");
+  // check the error is thrown for no primary keys (uncomment to check it works)
+  void testNoPks() {
+    // Table table_pk2 = new Table("Name","League","Points");
   }
 }
